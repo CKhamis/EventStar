c18f0e0db5308356456813573331ffe91a38fc3c
Constantine Khamis <costikhamis@gmail.com>
2024-10-03 00:12:43 -0700

    Added various user account classes

---
+++ src/main/java/com/costi/COEW/Config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.costi.COEW.Config;
+
+import com.costi.COEW.Model.User;
+import com.costi.COEW.Service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/interpersonal/**").authenticated()
+                        .requestMatchers("/api/interpersonal/**").hasAuthority(User.UserRole.OWNER.toString())
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/")
+                )
+                .logout(form -> form
+                        .logoutUrl("/logout")
+                );
+
+        return http.build();
+    }
+
+    private UserService userService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder());
+
+        return authProvider;
+    }
+}
+
---
+++ src/main/java/com/costi/COEW/Service/UserService.java
@@ -0,0 +1,24 @@
+package com.costi.COEW.Service;
+
+import com.costi.COEW.Model.User;
+import com.costi.COEW.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow();
+    }
+
+    public void save(User user){
+        userRepository.save(user);
+    }
+}
