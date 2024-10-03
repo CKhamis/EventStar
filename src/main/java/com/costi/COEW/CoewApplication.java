6cde2dea7f960ec8d604a289d9d1b32f1aea528b
Constantine Khamis <costikhamis@gmail.com>
2024-10-02 23:43:53 -0700

    Initial commit. Program can run and access Costi Online's database.

---
+++ src/main/java/com/costi/COEW/CoewApplication.java
@@ -0,0 +1,13 @@
+package com.costi.COEW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/costi/COEW/CoewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costi.COEW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
