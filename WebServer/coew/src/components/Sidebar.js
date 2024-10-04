e16fecfaa4edfcdc9805c20e731524750c5e8385
Constantine Khamis <costikhamis@gmail.com>
2024-10-04 14:01:48 -0700

    Removed the global stylesheet. Added tooltips

--- WebServer/coew/src/components/Sidebar.js
+++ WebServer/coew/src/components/Sidebar.js
@@ -1,16 +1,21 @@
+"use client";
 import Image from 'next/image';
+import 'react-tooltip/dist/react-tooltip.css'
+import { Tooltip } from 'react-tooltip'
 
 const Sidebar = () => {
     return (
         <div className="fixed top-0 w-16 h-screen bg-gray-900 left-0 flex flex-col">
             <ul className="flex-grow">
-                <li className="mb-4">
+                <li className="mb-4" data-tooltip-id="my-tooltip" data-tooltip-content="Home" data-tooltip-place="right">
                     <a href="http://costionline.com" className="decoration-0" role="button">
                         <Image src="/icons/Logo.svg" alt="logo" className="aspect-square p-2.5" width={100}
                                height={100}/>
                     </a>
+                    <Tooltip id="my-tooltip" />
                 </li>
 
+                <SideBarIcon iconSrc="/icons/Logo.svg" text="rat" url="/" />
                 <SideBarIcon iconSrc="/icons/Logo.svg" text="rat" url="/" />
 
             </ul>
@@ -23,17 +28,12 @@ const Sidebar = () => {
 }
 
 const SideBarIcon = ({iconSrc, url, text}) => (
-    <li className="border-b border-gray-600 group">
+    <li className="border-b border-gray-600 group" data-tooltip-id="my-tooltip" data-tooltip-content={text} data-tooltip-place="right">
         <a href={url} className="decoration-0" role="button">
             <Image src={iconSrc} alt="logo" className="aspect-square p-2.5" width={100}
                    height={100}/>
         </a>
-        <span className="absolute w-auto left-20 shadow-md
-        text-white bg-gray-900
-        text-xs font-bold opacity-50
-        transition-all duration-200 origin-left group-hover:opacity-100">
-            {text}
-        </span>
+        <Tooltip id="my-tooltip" />
     </li>
 );
 
