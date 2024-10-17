c1284910db5679b2b8e65cdd6a18c10573df30e1
Constantine Khamis <costikhamis@gmail.com>
2024-10-17 21:24:09 +0900

    fixed password message

--- WebServer/coew/src/lib/zod.ts
+++ WebServer/coew/src/lib/zod.ts
@@ -5,6 +5,5 @@ export const signInSchema = object({
         .min(6, "Email is required")
         .email("Invalid email address"),
     password: string({ required_error: "Password is required" })
-        .min(1, "Password is required")
-        .min(8, "Password is required"),
+        .min(1, "Password must be over 8 characters")
 })
